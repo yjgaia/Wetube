cfa86e3ebec9d94cda9c314c041b9ccf5e09499d
Hanul <hanul@hanul.me>
2015-11-14 22:03:50 +0900

    Youtube API 적용 및 검색 버튼 추가

--- app.js
+++ app.js
@@ -8,18 +8,7 @@
 		var pages = document.querySelector('iron-pages');
 		var userInfo = ref.getAuth();
 
-		var arr = [];
-
-		[1, 2, 3].forEach(function(i) {
-
-			arr.push({
-				id : 'id-' + i,
-				title : '영상 제목 ' + i,
-				description : '영상 설명 ' + i
-			});
-		});
-
-		app.videos = arr;
+		app.videos = [];
 
 		var list = document.querySelector('#list');
 		list.style.display = 'none';
@@ -38,12 +27,17 @@
 
 		app.googleLoggedIn = function() {
 
+			// 유저 정보가 없으면
 			if (userInfo === null) {
+
+				// 받아온 유저 정보를 대입한다.
 				userInfo = app.userInfo;
 
+				// 로그인 버튼을 숨기기, 로그인 창을 닫는다.
 				document.querySelector('#login firebase-google-login-button').style.display = 'none';
 				document.querySelector('#login').close();
 
+				// 로그인 된 이후 처리
 				logined();
 			}
 		};
@@ -59,16 +53,16 @@
 		};
 
 		app.view = function(e) {
-			
-			arr.forEach(function(data) {
+
+			app.videos.forEach(function(data) {
 				if (data.id === e.currentTarget.dataId) {
 					app.currentData = data;
 				}
 			});
-			
+
 			savedScrollY = window.pageYOffset;
 			window.scrollTo(0, savedScrollY);
-			
+
 			pages.selected = 1;
 		};
 
@@ -76,9 +70,69 @@
 			pages.selected = 0;
 			window.scrollTo(0, savedScrollY);
 		};
-		
+
 		app.inviteUser = function() {
 			document.querySelector('#users-dialog').open();
 		};
+
+		app.search = function(searchText) {
+
+			if (searchText === undefined || searchText === '') {
+
+				fetch('https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&key=' + Secure.youtubeKey + '&part=snippet&maxResults=10', {
+					headers : {
+						"Content-type" : "application/json"
+					}
+				}).then(function(response) {
+					return response.json();
+					// This returns a promise
+				}).then(function(responseObject) {
+					if (responseObject.error === undefined) {
+						responseObject.items.forEach(function(itemInfo, i) {
+							if (itemInfo.kind === 'youtube#video') {
+								app.push('videos', {
+									id : itemInfo.id,
+									title : itemInfo.snippet.title.length > 48 ? itemInfo.snippet.title.substring(0, 46) + '...' : itemInfo.snippet.title,
+									description : itemInfo.snippet.description,
+									channelTitle : itemInfo.snippet.channelTitle,
+									handle : itemInfo.id,
+									thumbnail : itemInfo.snippet.thumbnails.medium.url
+								});
+							}
+						});
+					}
+				}).catch(function(err) {
+					// An error occured parsing Json
+				});
+			} else {
+				fetch('https://www.googleapis.com/youtube/v3/search?q=' + searchText + '&key=' + Secure.youtubeKey + '&part=snippet&maxResults=10', {
+					headers : {
+						"Content-type" : "application/json"
+					}
+				}).then(function(response) {
+					return response.json();
+					// This returns a promise
+				}).then(function(responseObject) {
+					if (responseObject.error === undefined) {
+						responseObject.items.forEach(function(itemInfo, i) {
+							if (itemInfo.id.kind === 'youtube#video') {
+								app.push('videos', {
+									id : itemInfo.id,
+									title : itemInfo.snippet.title.length > 48 ? itemInfo.snippet.title.substring(0, 46) + '...' : itemInfo.snippet.title,
+									description : itemInfo.snippet.description,
+									channelTitle : itemInfo.snippet.channelTitle,
+									handle : itemInfo.id.videoId,
+									thumbnail : itemInfo.snippet.thumbnails.medium.url
+								});
+							}
+						});
+					}
+				}).catch(function(err) {
+					// An error occured parsing Json
+				});
+			}
+		};
+		
+		app.search();
 	});
-})(document); 
+})(document);

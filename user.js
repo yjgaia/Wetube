8865426ba3d51fd1e575c92372b3e5e4af1ace31
hanul <hanul@hanul.me>
2015-11-20 01:28:45 +0900

    채팅 기능 구현

--- user.js
+++ user.js
@@ -144,10 +144,101 @@ function injectUserFuncs(app) {
 		firebase.child('add-users').child(userInfo.uid).child(e.currentTarget.dataId).remove();
 	};
 	
+	var wantUserId;
+	var onInviteAccepted;
+	var inviteRoomRef;
+	var onRemoveInviteRoom;
+	var chatRef;
+	var onChat;
+	
+	clearRoom = function() {
+		
+		if (inviteRoomRef !== undefined) {
+			chatRef.off('child_added', onChat);
+			inviteRoomRef.off('child_removed', onRemoveInviteRoom);
+			inviteRoomRef.remove();
+			inviteRoomRef = undefined;
+			chatRef = undefined;
+			onChat = undefined;
+		}
+		
+		if (onInviteAccepted !== undefined) {
+			firebase.child('invite').child(wantUserId).child(userInfo.uid).off('child_added', onInviteAccepted);
+			onInviteAccepted = undefined;
+		}
+		wantUserId = undefined;
+	};
+	
+	startChat = function(inviteUserId, targetUserId, opponentUserName) {
+		
+		// 유저 초대 화면을 숨기고, 채팅 화면을 연다.
+		document.querySelector('#invite-user-form').hidden = true;
+		document.querySelector('#chat-list').hidden = false;
+		document.querySelector('#chat-form').hidden = false;
+		
+		app.chatInfos = [];
+		inviteRoomRef = firebase.child('invite').child(targetUserId);
+		
+		// 상대가 방을 나가 방이 파기되었을 때
+		onRemoveInviteRoom = inviteRoomRef.on('child_removed', function(snap) {
+			
+			if (snap.key() === inviteUserId) {
+				
+				app.push('chatInfos', {
+					isChat : false,
+					isLeft : true,
+					userName : opponentUserName
+				});
+				
+				clearRoom();
+				
+				document.querySelector('#chat-form').hidden = true;
+				
+				setTimeout(function() {
+					document.querySelector('#invite-user-form').hidden = false;
+					document.querySelector('#chat-list').hidden = true;
+				}, 3000);
+			}
+		});
+		
+		chatRef = inviteRoomRef.child(inviteUserId).child('chats');
+		
+		// 메시지가 왔을 때
+		onChat = chatRef.on('child_added', function(snap) {
+			
+			var info = snap.val();
+			
+			app.push('chatInfos', {
+				isChat : info.isChat,
+				userId : info.userId,
+				userName : info.userId === userInfo.uid ? (info.isChat === true ? '나' : userInfo.google.displayName) : opponentUserName,
+				nameSpanClass : info.userId === userInfo.uid ? 'me' : 'other',
+				message : info.message
+			});
+		});
+	};
+	
+	app.chat = function() {
+		if (app.chatMessage !== '') {
+			chatRef.push({
+				isChat : true,
+				userId : userInfo.uid,
+				message : app.chatMessage
+			});
+			app.chatMessage = '';
+		}
+	};
+
+	app.checkChatKey = function(e) {
+		if (e.keyCode === 13) {
+			app.chat();
+		}
+	};
+	
 	// 유저 초대하기
 	app.inviteUser = function(e) {
 		if (userInfo.nowVideoId !== undefined && app.currentData !== undefined) {
-			var wantUserId = e.currentTarget.dataId;
+			wantUserId = e.currentTarget.dataId;
 			firebase.child('invite').child(wantUserId).child(userInfo.uid).set({
 				videoId : userInfo.nowVideoId,
 				videoTitle : app.currentData.title,
@@ -163,6 +254,23 @@ function injectUserFuncs(app) {
 			// 초대 완료 알림 출력
 			app.invitedTargetUserName = e.currentTarget.dataName;
 			document.querySelector('#invited-toast').show();
+			
+			// 채팅 시작
+			startChat(userInfo.uid, wantUserId, app.invitedTargetUserName);
+			
+			// 초대를 수락했을때
+			if (onInviteAccepted !== undefined) {
+				firebase.child('invite').child(wantUserId).child(userInfo.uid).off('child_added', onInviteAccepted);
+			}
+			onInviteAccepted = firebase.child('invite').child(wantUserId).child(userInfo.uid).on('child_added', function(snap) {
+				if (snap.key() === 'isAccepted' && snap.val() === true) {
+					chatRef.push({
+						isChat : false,
+						isLeft : false,
+						userId : wantUserId
+					});
+				}
+			});
 		}
 	};
 	
@@ -170,12 +278,18 @@ function injectUserFuncs(app) {
 	app.acceptInvite = function() {
 		if (app.inviteVideoData !== undefined) {
 			app.view(app.inviteVideoData);
-			app.removeInvite();
+			document.querySelector('#invite-user-toast').hide();
+			
+			firebase.child('invite').child(userInfo.uid).child(app.inviteUserId).update({
+				isAccepted : true
+			});
+			
+			startChat(app.inviteUserId, userInfo.uid, app.inviteUserName);
 		}
 	};
 	
-	// 초대 삭제
-	app.removeInvite = function() {
+	// 초대 거절
+	app.dismissInvite = function() {
 		firebase.child('invite').child(userInfo.uid).child(app.inviteUserId).remove();
 		
 		delete app.inviteUserName;
@@ -195,10 +309,17 @@ function injectUserFuncs(app) {
 	
 	// 비디오 방 퇴장
 	app.exitRoom = function() {
+		
 		firebase.child('users').child(userInfo.uid).update({
 			nowVideoId : null,
 			wantUserId : null
 		});
 		delete userInfo.nowVideoId;
+		
+		clearRoom();
+		
+		document.querySelector('#chat-form').hidden = true;
+		document.querySelector('#invite-user-form').hidden = false;
+		document.querySelector('#chat-list').hidden = true;
 	};
 }
